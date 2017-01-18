452edeeb0f8c46f0bc22a0d36f311f7bfba3c6cf
Ophelia Yin <owyowy1@gmail.com>
2017-01-17 16:10:19 -0800

    lab11

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -2,6 +2,10 @@
 
 (function(module) {
   const aboutController = {};
+  aboutController.init = function(){
+    $('.tab-content').hide();
+    $('#about').show();
+  };
 
   // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
 
--- starter-code/public/scripts/controllers/articleController.js
+++ starter-code/public/scripts/controllers/articleController.js
@@ -2,10 +2,14 @@
 
 (function(module) {
   const articleController = {};
+  articleController.init = function(){
+  Article.fetchAll(articleView.initIndexPage);
+    $('.tab-content').hide();
+    $('#articles').show();
+  };
   // TODO: Setup a function that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
   // Also be sure to hide all the main section elements, and reveal the #articles section:
-
 
   module.articleController = articleController;
 })(window);
